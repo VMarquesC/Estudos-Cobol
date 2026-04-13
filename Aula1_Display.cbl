1807d69c4005a37118356db596a6fec50729a3b8
Vitor Adriano <vitor102017@hotmail.com>
2026-04-13 11:43:42 -0300

    Add files via upload

---
+++ Aula1_Display.cbl
@@ -0,0 +1,16 @@
+*>****************************************************************
+*> Author:
+*> Date:
+*> Purpose:
+*> Tectonics: cobc
+*>****************************************************************
+IDENTIFICATION DIVISION.
+PROGRAM-ID. YOUR-PROGRAM-NAME.
+DATA DIVISION.
+FILE SECTION.
+WORKING-STORAGE SECTION.
+PROCEDURE DIVISION.
+MAIN-PROCEDURE.
+    DISPLAY "Olá, fulano!" 45
+    STOP RUN.
+END PROGRAM YOUR-PROGRAM-NAME.
